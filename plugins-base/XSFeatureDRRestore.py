61939239d6e404153b9c9175227a9f7b12f05a42
Anil Madhavapeddy <anil.madhavapeddy@eu.citrix.com>
2008-06-17 19:46:59 +0100

    CA-16731: finish up the DR Restore menu with SR selection, probing support, date selection, and "selecting all VMs/only VMs in SR" and giving user option to do a dry run first.
    No admin shall ever weep again after destroying their VBD associations, assuming this feature doesnt do that for them.

--- plugins-base/XSFeatureDRRestore.py
+++ plugins-base/XSFeatureDRRestore.py
@@ -10,6 +10,108 @@ if __name__ == "__main__":
     
 from XSConsoleStandard import *
 
+class DRRestoreSelection(Dialogue):
+
+    def __init__(self, date_choices, vdi_uuid, sr_uuid):
+        Dialogue.__init__(self)
+    
+        pane = self.NewPane(DialoguePane(self.parent))
+        pane.TitleSet("")
+        pane.AddBox()
+       
+        choices = []
+        self.vdi_uuid = vdi_uuid
+        self.sr_uuid = sr_uuid
+        self.date_choices = date_choices.splitlines()
+        index = 0
+        for choice in self.date_choices:
+            cdef = ChoiceDef(choice, lambda i=index: self.HandleTestChoice(i))
+            index = index + 1
+            choices.append(cdef)
+
+        self.testMenu = Menu(self, None, "", choices)
+
+        self.methodMenu = Menu(self, None, "", [
+           ChoiceDef("Only VMs on this SR", lambda: self.HandleMethodChoice('sr', False)),
+           ChoiceDef("All VM Metadata", lambda: self.HandleMethodChoice('all', False)),
+           ChoiceDef("Only VMs on this SR (dry run)", lambda: self.HandleMethodChoice('all', True)),
+           ChoiceDef("All VM Metadata (dry run)", lambda: self.HandleMethodChoice('all', True)),
+        ])
+        self.state = 'LISTDATES'
+        self.UpdateFields()
+    
+    def UpdateFieldsLISTDATES(self):
+        pane = self.Pane()
+        pane.ResetFields()
+        
+        pane.TitleSet("Available Metadata Backups")
+        pane.AddTitleField(Lang("Select Metadata Backup to Restore From"))
+        pane.AddMenuField(self.testMenu)
+        pane.AddKeyHelpField( { Lang("<Enter>") : Lang("OK"), Lang("<Esc>") : Lang("Cancel") } )
+       
+    def UpdateFieldsCHOOSERESTORE(self):
+        pane = self.Pane()
+        pane.ResetFields()
+
+        pane.TitleSet("Restore Backup from " + self.chosen_date)
+        pane.AddTitleField("Select the set of VMs to restore from " + self.chosen_date)
+        pane.AddMenuField(self.methodMenu)
+        pane.AddKeyHelpField( { Lang("<Enter>") : Lang("Restore VMs"), Lang("<Esc>") : Lang("Cancel") } )
+
+    def UpdateFields(self):
+        self.Pane().ResetPosition()
+        getattr(self, 'UpdateFields'+self.state)() # Despatch method named 'UpdateFields'+self.state
+
+    def HandleTestChoice(self,  inChoice):
+        self.chosen_date = self.date_choices[inChoice]
+        self.state = 'CHOOSERESTORE'
+        self.UpdateFields()
+
+    def HandleMethodChoice(self, inChoice, dryRun):
+        if inChoice != 'sr' and inChoice != 'all':
+            Layout.Inst().PopDialogue()
+            Layout.Inst().PushDialogue(InfoDialogue(Lang("Internal Error, unexpected choice: " + inChoice)))
+        else:
+            chosen_mode = inChoice
+            if dryRun:
+              dry_flag="-n "
+            else:
+              dry_flag=""
+            Layout.Inst().TransientBanner(Lang("Restoring VM Metadata.  This may take a few minutes..."))
+            command = "/opt/xensource/bin/xe-restore-metadata -y " + dry_flag +" -u " + self.sr_uuid + " -x " + self.vdi_uuid + " -d " + self.chosen_date + " -m " + chosen_mode
+            status, output = commands.getstatusoutput(command)
+            status = os.WEXITSTATUS(status)
+            Layout.Inst().PopDialogue()
+            if status == 0:
+                Layout.Inst().PushDialogue(InfoDialogue(Lang("Metadata Restore Succeeded: ") + output))
+            else:
+                Layout.Inst().PushDialogue(InfoDialogue(Lang("Metadata Restore Failed: ") + output))
+
+    def HandleKey(self, inKey):
+        handled = False
+        if hasattr(self, 'HandleKey'+self.state):
+            handled = getattr(self, 'HandleKey'+self.state)(inKey)
+
+        if not handled and inKey == 'KEY_ESCAPE':
+            Layout.Inst().PopDialogue()
+            handled = True
+
+        return handled
+ 
+    def HandleKeyLISTDATES(self, inKey):
+        handled = self.testMenu.HandleKey(inKey)
+        if not handled and inKey == 'KEY_LEFT':
+            Layout.Inst().PopDialogue()
+            handled = True
+        return handled
+
+    def HandleKeyCHOOSERESTORE(self, inKey):
+        handled = self.methodMenu.HandleKey(inKey)
+        if not handled and inKey == 'KEY_LEFT':
+            Layout.Inst().PopDialogue()
+            handled = True
+        return handled
+
 class DRRestoreDialogue(SRDialogue):
     def __init__(self):
 
@@ -41,8 +143,7 @@ class DRRestoreDialogue(SRDialogue):
             status = os.WEXITSTATUS(status)
             if status != 0:
                 raise Exception(output)
-
-            Layout.Inst().PushDialogue(InfoDialogue(output))
+            Layout.Inst().PushDialogue(DRRestoreSelection(output, vdi_uuid, sr_uuid))
         except Exception, e:
             Layout.Inst().PushDialogue(InfoDialogue( Lang("Metadata Restore failed: ")+str(e)))
         Data.Inst().Update()
