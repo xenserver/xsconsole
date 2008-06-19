d781323b3817bb1dea447429793f6fa18892cab5
Andy Southgate <andy.southgate@eu.citrix.com>
2008-06-19 12:11:26 +0100

    Disallow detach/destroy for local storage

--- plugins-base/XSFeatureSRCommon.py
+++ plugins-base/XSFeatureSRCommon.py
@@ -151,8 +151,7 @@ class SRUtils: # FIXME: Name clash
     @classmethod
     def IsDetachable(cls, inSR):
         # Use same criteria as XenCenter for detach (from IsDetachable in SR.cs)
-        # Also allow lvm to detach local disks
-        if 'forget' in inSR.allowed_operations() and inSR.type() in ('lvm', 'iso', 'lvmoiscsi', 'nfs', 'netapp', 'lvmohba', 'equal'):
+        if 'forget' in inSR.allowed_operations() and inSR.type() in ('iso', 'lvmoiscsi', 'nfs', 'netapp', 'lvmohba', 'equal'):
             retVal = True
         else:
             retVal = False
